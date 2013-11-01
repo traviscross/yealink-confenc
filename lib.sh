0b71bc618720857cca1935cb188f7c721cb6338b
Travis Cross <tc@traviscross.com>
2013-11-01 23:55:40 +0000

    Be a bit more consistent with API

--- lib.sh
+++ lib.sh
@@ -7,30 +7,30 @@
 # the encryption tool and writes the result to a second file, also
 # using AES-128-ECB.
 
-decrypt_keyfile () {
+decrypt_key () {
   local keyfile="$1"
   openssl enc -aes-128-ecb -d -nopad -in $keyfile \
     -K $(printf 'EKs35XacP6eybA25' | xxd -p)
 }
-#decrypt_keyfile test_Security.enc
+#decrypt_key test_Security.enc
 
 decrypt_cfg () {
   local cfg="$1" key="$2"
   openssl enc -aes-128-ecb -d -nopad -in $cfg \
     -K $(printf "$key" | xxd -p)
 }
-#decrypt_cfg test.cfg $(decrypt_keyfile test_Security.enc)
+#decrypt_cfg test.cfg $(decrypt_key test_Security.enc)
 
 encrypt_cfg () {
   local cfg="$1" key="$2"
   openssl enc -aes-128-ecb -nopad -in $cfg \
     -K $(printf "$key" | xxd -p)
 }
-#(encrypt_cfg testpt.cfg $(decrypt_keyfile test_Security.enc) | diff test.cfg -) && echo "Success"
+#(encrypt_cfg testpt.cfg $(decrypt_key test_Security.enc) | diff test.cfg -) && echo "Success"
 
 encrypt_key () {
   local key="$1"
   printf "$key" | openssl enc -aes-128-ecb -nopad \
     -K $(printf 'EKs35XacP6eybA25' | xxd -p)
 }
-#(encrypt_key $(decrypt_keyfile test_Security.enc) | diff test_Security.enc -) && echo "Success"
+#(encrypt_key $(decrypt_key test_Security.enc) | diff test_Security.enc -) && echo "Success"

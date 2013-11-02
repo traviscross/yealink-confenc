ca91b5e00cbfe2146b4feaa07501128fe7b28796
Travis Cross <tc@traviscross.com>
2013-11-02 00:16:27 +0000

    Remove testing/usage comments

--- lib.sh
+++ lib.sh
@@ -12,25 +12,21 @@ decrypt_key () {
   openssl enc -aes-128-ecb -d -nopad -in $keyfile \
     -K $(printf 'EKs35XacP6eybA25' | xxd -p)
 }
-#decrypt_key test_Security.enc
 
 decrypt_cfg () {
   local cfg="$1" key="$2"
   openssl enc -aes-128-ecb -d -nopad -in $cfg \
     -K $(printf "$key" | xxd -p)
 }
-#decrypt_cfg test.cfg $(decrypt_key test_Security.enc)
 
 encrypt_cfg () {
   local cfg="$1" key="$2"
   openssl enc -aes-128-ecb -nopad -in $cfg \
     -K $(printf "$key" | xxd -p)
 }
-#(encrypt_cfg testpt.cfg $(decrypt_key test_Security.enc) | diff test.cfg -) && echo "Success"
 
 encrypt_key () {
   local key="$1"
   printf "$key" | openssl enc -aes-128-ecb -nopad \
     -K $(printf 'EKs35XacP6eybA25' | xxd -p)
 }
-#(encrypt_key $(decrypt_key test_Security.enc) | diff test_Security.enc -) && echo "Success"
